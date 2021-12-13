7318e5bac8c00cc03e59379afe38e96e334784cf
Nikolay Rusev <nikolay.z.rusev@gmail.com>
2021-12-13 13:57:09 +0200

    add simple example of react-final-form

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Form, Field } from "react-final-form";
 
-function App() {
+const onSubmit = (values) => {
+  window.alert(JSON.stringify(values, 0, 2));
+};
+
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ padding: 15, margin: "auto", maxWidth: 600 }}>
+      <h2>Gravity Well Application Employment Form</h2>
+      <Form
+        onSubmit={onSubmit}
+        render={({ handleSubmit }) => (
+          <form onSubmit={handleSubmit}>
+            <div>
+              <label>First Name</label>
+              <Field
+                name="firstName"
+                component="input"
+                type="text"
+                placeholder="First Name"
+              />
+            </div>
+            <button type="submit">Submit</button>
+          </form>
+        )}
+      />
     </div>
   );
 }
-
-export default App;
